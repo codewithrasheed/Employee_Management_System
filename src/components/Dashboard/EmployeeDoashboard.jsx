40109d846e4884d3414c675402801dd4af2f161f
Codewithrasheed <rasheedahmed4845@gmail.com>
2024-11-03 09:40:55 +0500

    employeedashboard-update

--- src/components/Dashboard/EmployeeDoashboard.jsx
+++ src/components/Dashboard/EmployeeDoashboard.jsx
@@ -5,7 +5,7 @@ import TaskList from '../TaskList/TaskList'
 
 const EmployeeDoashboard = (props) => {
   return (
-    <div className='p-10 bg-[#1C1C1C] h-screen w-full employeedash'>
+    <div className='p-10 bg-[#1C1C1C]  employeedash'>
         <Header changeUser={props.changeUser} data={props.data} />
         <TaskListNumber data={props.data} />
         <TaskList data={props.data} />
--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -7,9 +7,9 @@ const Header = (props) => {
   
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>
+        <h1 className='text-2xl font-medium name'>Hello <br /> <span className='text-3xl username font-semibold'>
           {props.data ? props.data.username : "Rasheed" } 👋</span> </h1>
-        <button onClick={logOutUser} className='bg-red-600 px-5 py-2 rounded-md font-medium text-lg'>Log Out</button>
+        <button onClick={logOutUser} className='bg-red-600 px-5 py-2 rounded-md font-medium text-lg logout'>Log Out</button>
     </div>
   )
 }
